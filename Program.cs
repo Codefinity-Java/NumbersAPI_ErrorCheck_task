68f37a0346eb3152388b36c1f88a406cb9083f7f
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2024-04-04 20:49:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿public class NumbersFact
+{
+    public static async Task Main()
+    {
+        Console.WriteLine(">> This program gives you a random fact about any day of the year. ");
+
+        Console.Write("Enter the number of month (1-12): ");
+        int month = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of day: ");
+        int day = int.Parse(Console.ReadLine());
+
+        // Write code below this line
+        string url = $"http://numbersapi.com/{month}/{day}/date";
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+        string responseContent = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Fact: " + responseContent);
+        // Write code above this line
+    }
+}
